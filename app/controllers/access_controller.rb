4e323f70bc08aa577d4978aa739316086e3d8190
lessin <lessin@gmail.com>
2014-11-19 11:41:50 -0800

    controllers and views for users

--- app/controllers/access_controller.rb
+++ app/controllers/access_controller.rb
@@ -1,7 +1,39 @@
 class AccessController < ApplicationController
 
-def login
-end
+  before_action :confirm_logged_in, except: [:attempt_login, :login, :logout]
+  before_action :prevent_login_signup, only: [:login]
+
+  def login
+  end
+
+  def attempt_login
+    if params[:username].present? && params[:password].present?
+      found_user = User.where(username: params[:username]).first
+      if found_user
+        authorized_user = found_user.authenticate(params[:password])
+      end
+    end
+
+    if !found_user
+      flash.now[:alert] = "Invalid username"
+      render :login
+    elsif !authorized_user
+      flash.now[:alert] = "Invalid password"
+      render :login
+    else
+      session[:user_id] = authorized_user.id
+      session[:admin] = authorized_user.admin
+      flash[:success] = "You are now logged in."
+      redirect_to users_path
+    end
+  end
+
+  def logout
+    session[:user_id] = nil
+    session[:admin] = nil
+    flash[:notice] = "Logged out"
+    redirect_to login_path
+  end
 
 
 end
