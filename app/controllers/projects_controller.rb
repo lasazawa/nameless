abfe48fcd24a095b361f9320aa5b7f602e57feba
= <lasazawa@gmail.com>
2014-11-19 15:39:24 -0800

    connecting users with projects and gitignore

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,5 +1,9 @@
 class ProjectsController < ApplicationController
 
+before_action :confirm_logged_in, except: [:show]
+before_action :current_user
+
+
 def new
   @project = Project.new
 end
@@ -8,7 +12,7 @@ def create
   @project = Project.create(project_params)
   if @project.save
     flash[:success] = "Your new project has been saved!"
-    redirect_to user_project_path(1, @project.id)
+    redirect_to user_project_path(@current_user.id, @project.id)
   end
 end
 
@@ -25,7 +29,7 @@ def update
   @project.update_attributes(project_params)
   if @project.save
     flash[:success] = "Your project has been saved"
-    redirect_to user_project_path(1, @project.id)
+    redirect_to user_project_path(@current_user.id, @project.id)
   else
     render :show
   end
