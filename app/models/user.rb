1ddb254a82618cf8fff27e3954b9d1d464e31922
Joshua Notowitz <Joshua@Joshuas-MacBook-Pro.local>
2014-11-19 11:41:57 -0800

    User table additions

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,16 @@
 class User < ActiveRecord::Base
+	has_secure_password
+
 	has_many :projects
 	has_many :votes
 	has_many :names, :through => :votes
 	has_many :usertags
 	has_many :tags, :through => :usertags
+
+	validates :username,
+		presence: true,
+		uniquness: true
+	validates :password,
+		presence: true,
+		length: {minimum: 5}
 end
