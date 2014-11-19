817864e3b007eea31978443d750872f37894617b
lessin <lessin@gmail.com>
2014-11-19 11:55:46 -0800

    fixing routes for login and adding put

--- db/schema.rb
+++ db/schema.rb
@@ -41,6 +41,14 @@ ActiveRecord::Schema.define(version: 20141119175359) do
   end
 
   create_table "users", force: true do |t|
+    t.string   "username"
+    t.string   "password"
+    t.string   "password_digest"
+    t.string   "firstname"
+    t.string   "lastname"
+    t.integer  "age"
+    t.string   "city"
+    t.string   "picurl"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
