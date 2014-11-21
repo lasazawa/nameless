117447484bfaf860189d62ed9d8d1ed2ae629ffd
Joshua Notowitz <joshnotowitz@gmail.com>
2014-11-21 11:30:26 -0800

    new user edirect and navbar change

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,7 +13,7 @@ class UsersController < ApplicationController
     if(@user.save)
       session[:user_id] = @user.id
       flash[:success] = "You are now logged in!"
-      redirect_to user_path(@user.id)
+      redirect_to edit_user_path(@user.id)
     else
       render :new
     end
