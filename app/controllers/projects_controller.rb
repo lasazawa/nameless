130ceae53d3278ab01f4c63206704dbf0482f496
Joshua Notowitz <joshnotowitz@gmail.com>
2014-11-21 14:02:13 -0800

    new tags showing on page without refresh

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -63,7 +63,6 @@ def vote
   name.liked_by user
   names = project.names
   votes = []
-  binding.pry
   names.each do |v|
     vote = {:name => v.name, :totalvotes => v.votes_for.size}
     votes << vote
